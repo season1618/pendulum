196c89b0eee06481d559ace1a9df51aa8d1a62e9
season1618 <seasonminority314@gmail.com>
2024-10-25 03:10:09 +0900

    型を一般化

--- src/Ode.hs
+++ src/Ode.hs
@@ -3,7 +3,7 @@ module Ode where
 -- solve ds/dt = f(s)
 
 -- s_n+1 = s_n + f(s_n) * h
-euler :: ([Float] -> [Float]) -> Float -> [Float] -> [Float]
+euler :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
 euler f dt state = do
     let dstate = f state
     zipWith (\s u -> s + u * dt) state dstate
