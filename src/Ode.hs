e223f8b5e0eaf02cd29fc79d7a806ad9d8f6463a
season1618 <seasonminority314@gmail.com>
2024-10-25 04:12:06 +0900

    1次ルンゲ＝クッタ法，オイラー法

--- src/Ode.hs
+++ src/Ode.hs
@@ -3,10 +3,13 @@ module Ode where
 -- solve ds/dt = f(s)
 
 -- s_n+1 = s_n + f(s_n) h
-euler :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
-euler f dt state = do
+rk1 :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
+rk1 f dt state = do
     let dstate = f state
     zipWith (\s u -> s + u * dt) state dstate
+
+euler :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
+euler = rk1
 
 -- s_n+1 = s_n + { (1-1/2a) f(s_n) + 1/2a f(s_n + af(s_n)h) } h
 rk2 :: Float -> ([Float] -> [Float]) -> Float -> [Float] -> [Float] -- Floating a => a -> ([a] -> [a]) -> a -> [a] -> [a]
