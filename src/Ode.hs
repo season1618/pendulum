d499c95071759ceb750454411f4e7a232fed8a70
season1618 <seasonminority314@gmail.com>
2024-10-25 16:59:00 +0900

    リスト演算

--- src/Ode.hs
+++ src/Ode.hs
@@ -1,12 +1,15 @@
 module Ode where
 
+instance Num a => Num [a] where
+    (+) = zipWith (+)
+    (-) = zipWith (-)
+    abs = map abs
+
 -- solve ds/dt = f(s)
 
 -- s_n+1 = s_n + f(s_n) h
 rk1 :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
-rk1 f dt state = do
-    let dstate = f state
-    zipWith (\s u -> s + u * dt) state dstate
+rk1 f dt state = state + map (* dt) (f state)
 
 euler :: Num a => ([a] -> [a]) -> a -> [a] -> [a]
 euler = rk1
@@ -14,11 +17,10 @@ euler = rk1
 -- s_n+1 = s_n + { (1-1/2a) f(s_n) + 1/2a f(s_n + af(s_n)h) } h
 rk2 :: Float -> ([Float] -> [Float]) -> Float -> [Float] -> [Float] -- Floating a => a -> ([a] -> [a]) -> a -> [a] -> [a]
 rk2 a f dt s = do
-    let (.+.) = zipWith (+)
     let (.*) x ys = map (x *) ys
     let (*.) xs y = map (* y) xs
     let fs = f s
-    s .+. ( ( ((1-1/(2*a)) .* fs) .+. ((1/(2*a)) .* f (s .+. (a .* fs *. dt))) ) *. dt)
+    s + ( ((1-1/(2*a)) .* fs) + ((1/(2*a)) .* f (s + (a .* fs *. dt))) ) *. dt
 
 midpoint :: ([Float] -> [Float]) -> Float -> [Float] -> [Float]
 midpoint = rk2 0.5
@@ -32,26 +34,24 @@ heun = rk2 1
 -- k_3 = f(s_n + k_2 h/2)
 -- k_4 = f(s_n + k_3 h)
 rk4Classical :: ([Float] -> [Float]) -> Float -> [Float] -> [Float]
-rk4Classical f dt s = s .+. ((k1 .+. (2 .* k2) .+. (2 .* k3) .+. k4) *. (dt / 6)) where
-    (.+.) = zipWith (+)
+rk4Classical f dt s = s + (k1 + 2 .* k2 + 2 .* k3 + k4) *. (dt / 6) where
     (.*) x ys = map (x *) ys
     (*.) xs y = map (* y) xs
 
     k1 = f s
-    k2 = f $ s .+. (k1 *. (dt/2))
-    k3 = f $ s .+. (k2 *. (dt/2))
-    k4 = f $ s .+. (k3 *. dt)
+    k2 = f $ s + (k1 *. (dt/2))
+    k3 = f $ s + (k2 *. (dt/2))
+    k4 = f $ s + (k3 *. dt)
 
 loss :: [Float] -> [Float] -> Float
-loss a b = minimum $ map abs (zipWith (-) a b)
+loss a b = minimum $ abs (a - b)
 
 -- s_n+1 = s_n + f((s_n + s_n+1) / 2) h
 midpointImplicit :: ([Float] -> [Float]) -> Float -> [Float] -> [Float]
 midpointImplicit f dt s = iter s where
-    (.+.) = zipWith (+)
     (.*) x ys = map (x *) ys
     (*.) xs y = map (* y) xs
     iter :: [Float] -> [Float]
     iter sNext = do
-        let sNext' = s .+. (f ((s .+. sNext) *. 0.5) *. dt)
+        let sNext' = s + f ((s + sNext) *. 0.5) *. dt
         if loss sNext sNext' <= 1e-6 then sNext' else iter sNext'
